e43970be6363179d3e12c6771b5c7ee40a4f99f5
Ndi-Shema <f.shema@alustudent.com>
2023-02-19 13:43:14 +0200

    Move file from tests to main

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -9,11 +9,10 @@ CREATE USER
     IF NOT EXISTS 'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*;
+   ON `hbnb_test_db`.*
    TO 'hbnb_test'@'localhost';
    IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT
-   ON `performance_schema`.*;
+   ON `performance_schema`.*
    TO 'hbnb_test'@'localhost';
-   IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
