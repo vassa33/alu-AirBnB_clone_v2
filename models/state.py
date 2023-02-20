6881a3baf2453c9793bf103769141ce5ebd3481a
Ndi-Shema <f.shema@alustudent.com>
2023-02-20 13:45:06 +0200

    DBStorage

--- models/state.py
+++ models/state.py
@@ -5,4 +5,19 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(string(128), nullable=False)
+    cities = relationship('City', backref=backref('state', lazy='dynamic'), cascade='all, delete-orphan')
+
+    class FileStorage:
+        def __init__(self):
+            self.__file_path = 'file.json'
+            self.__objects = {}
+
+        @property
+        def cities(self):
+            return [city for city in self.__objects.values() if
+                    city.__class__.__name__ == 'City' and city.state_id == self.id]
+
+
