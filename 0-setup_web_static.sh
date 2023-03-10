645680a4db628f2c50a19b775b4cc64ca13a5e00
Vassa <p.otieno@alustudent.com>
2023-03-10 17:43:56 +0000

    updated 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
-# Update package list and install Nginx
-apt-get update
-apt-get -y install nginx
+# Sets up a web server for deployment of web_static.
 
-# Create necessary directories
+apt-get update
+apt-get install -y nginx
+
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-
-# Create fake HTML file
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
-# Create symbolic link
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership and group of directories
-chown -R ubuntu:ubuntu /data/
-chmod -R 755 /data/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Update Nginx configuration file
-sed -i '/listen 80 default_server;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n        index index.html;\n    }' /etc/nginx/sites-available/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Restart Nginx
-systemctl restart nginx
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-exit 0
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
