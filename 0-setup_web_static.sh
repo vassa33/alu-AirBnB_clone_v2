ae2a5bc8cff2a12c62663c078586dc11a0e7fbe5
Vassa <p.otieno@alustudent.com>
2023-03-10 18:06:12 +0000

    airbnb_clone-v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -25,7 +25,7 @@ printf %s "server {
     }
 
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 http://mydomainame.tech/;
     }
 
     error_page 404 /404.html;
