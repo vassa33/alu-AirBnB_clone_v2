15fe8691fde1ed7605b498e7f91d982c490f313d
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-03-10 13:31:08 +0300

    Checker 3

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,16 +7,16 @@ from fabric.api import local
 def do_pack():
     """Create a compressed archive of the web_static directory."""
     local("mkdir -p versions")
-    
+
     # Create the filename with the current timestamp
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"web_static_{timestamp}.tgz"
-    
+    filename = "web_static_{}.tgz".format(timestamp)
+
     # Compress the web_static directory to the archive file
-    result = local(f"tar -cvzf versions/{filename} web_static/")
-    
+    result = local("tar -cvzf versions/{} web_static/".format(filename))
+
     # Return the archive path if the compression was successful, otherwise return None
     if result.succeeded:
-        return f"versions/{filename}"
+        return "versions/{}".format(filename)
     else:
         return None
