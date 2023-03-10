c1c05812ebcd43e768bf0cf969798d7a89c2b9b9
vassa33 <peggyongoma@gmail.com>
2023-03-10 11:38:15 +0300

    Prepare web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,60 +1,34 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
-# Exit on error
-set -e
 
 # Update package list and install Nginx
-sudo apt-get update
-sudo apt-get install -y nginx
-
-# Remove existing Nginx default configuration file
-sudo rm /etc/nginx/sites-enabled/default || true
+apt-get update
+apt-get -y install nginx
 
 # Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
 # Create fake HTML file
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html >/dev/null
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
 # Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Set ownership and group of directories
-sudo chown -R ubuntu:ubuntu /data/
-sudo chmod -R 755 /data/
+chown -R ubuntu:ubuntu /data/
+chmod -R 755 /data/
 
 # Update Nginx configuration file
-sudo printf %s "\
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root /var/www/html;
-    index index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-        root /var/www/html;
-        internal;
-    }
-}" | sudo tee /etc/nginx/sites-available/default >/dev/null
-
-# Create symbolic link to enable the Nginx configuration
-sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+sed -i '/listen 80 default_server;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n        index index.html;\n    }' /etc/nginx/sites-available/default
 
 # Restart Nginx
-sudo systemctl restart nginx
+systemctl restart nginx
 
 exit 0
 
