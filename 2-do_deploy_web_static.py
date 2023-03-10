5bd9cdbb6a9f05329a04e3da44f879d5dbb10fd6
Vassa <p.otieno@alustudent.com>
2023-03-10 18:14:01 +0000

    airbnb_clone-v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,80 +1,49 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to web servers
-"""
+# Fabfile to distribute an archive to a web server.
 import os.path
-from fabric.api import env, local, put, run
-from datetime import datetime
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-env.user = 'ubuntu'
-env.hosts = ['3.90.0.75', '54.175.29.140']
-
-
-def do_pack():
-    """
-    Create a tar gzipped archive of the directory web_static.
-    """
-    try:
-        if not os.path.isdir("versions"):
-            os.makedirs("versions")
-        now = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_path = "versions/web_static_{}.tgz".format(now)
-        local("tar -cvzf {} web_static".format(archive_path))
-        return archive_path
-    except:
-        return None
+env.hosts = ["3.90.0.75", "54.175.29.140"]
 
 
 def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    Distributes an archive to web servers
-    """
-    if not os.path.exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    try:
-        archive_name = os.path.basename(archive_path)
-        no_ext = os.path.splitext(archive_name)[0]
-
-        # Upload archive to /tmp/ directory of the web server
-        put(archive_path, "/tmp/")
-
-        # Create directory to uncompress the archive
-        run("sudo mkdir -p /data/web_static/releases/{}".format(no_ext))
-
-        # Uncompress archive into the folder created
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_name, no_ext))
-
-        # Delete archive from web server
-        run("sudo rm /tmp/{}".format(archive_name))
-
-        # Move uncompressed files to its own folder
-        run("sudo mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/"
-            .format(no_ext, no_ext))
-
-        # Remove original web_static folder
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(no_ext))
-
-        # Delete symbolic link
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create new symbolic link
-        run("sudo ln -s /data/web_static/releases/{}/ "
-            "/data/web_static/current".format(no_ext))
-
-        # Add a new file to the deployment
-        run("sudo touch /data/web_static/releases/{}/index.html"
-            .format(no_ext))
-        run("sudo echo '<html><head></head><body>Holberton School</body></html>' "
-            "| sudo tee /data/web_static/releases/{}/index.html"
-            .format(no_ext))
-
-        print("New version deployed!")
-        return True
-
-    except Exception as e:
-        print(e)
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
