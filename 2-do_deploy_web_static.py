459c2aec398fdaf2784d6d9dbd25f43a4a5ddbcc
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-03-10 12:55:58 +0300

    Fix do_pack( )

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,19 +12,17 @@ from datetime import datetime
 env.user = 'ubuntu'
 env.hosts = ['3.90.0.75', '54.175.29.140']
 
+
 def do_pack():
-    """Create a compressed archive of the web_static directory."""
-    # Create the versions directory if it doesn't exist
-    local("mkdir -p versions")
-    # Create the filename with the current timestamp
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"web_static_{timestamp}.tgz"
-    # Compress the web_static directory to the archive file
-    result = local(f"tar -cvzf versions/{filename} web_static/")
-    # Return the archive path if the compression was successful, otherwise return None
-    if result.succeeded:
-        return f"versions/{filename}"
-    else:
+    """Create a tar gzipped archive of the directory web_static."""
+    try:
+        if not os.path.isdir("versions"):
+            os.makedirs("versions")
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(now)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
         return None
 
 
